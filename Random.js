5b8be9afd4101db66be33d78b2922b11ac7d4a34
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 15:51:27 +0200

    Add files via upload

--- Random.js
+++ Random.js
@@ -1,20 +1,12 @@
 var CurrentID = 0
-var OldID = CurrentID
-
-function handleID(newId) {
-    var newerId = newId
-    OldID = newerId++;
-}
-
 window.TrelloPowerUp.initialize({
-    // Start adding handlers for your capabilities here!
     'card-buttons': function (t, options) {
       return [
         {
           icon: "https://i.ibb.co/6sbknH0/logo-stroke-32pt-style-2-no-stroke.png",
           text: 'Hello World!',
           callback: function (t) {
-            return t.card().then((card) => alert(handleID(CurrentID)))
+            return t.card().then((card) => alert(toString(CurrentID)))
           }
         }
       ];
