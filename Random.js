5d4d3a3fa82d63783a704b4aedae159c4f2b9f67
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-17 09:49:31 +0200

    Version 2.02

--- Random.js
+++ Random.js
@@ -30,7 +30,7 @@ window.TrelloPowerUp.initialize({
     },
     "card-detail-badges": function (t, opts) {
       return t.card("name").get("name").then(function (cardName) {
-         alert("We just loaded the card name for fun: " + cardName);
+         alert("Your ticket has an ID of " + CurrentID);
           return [
             {
               dynamic: function () {
